ce1497444e78569fdfeeba3a38e7b96dc4c95f19
tyler <1291571584@qq.com>
2022-08-05 15:21:07 +0800

    equal line commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="info">
+    <!-- <Voice ></Voice> -->
+  </div>
+</template>
+
+<script>
+
+import Voice from "@/components/voice";
+export default {
+  components: {
+    Voice,
+  },
+};
+</script>
+
+<style></style>
